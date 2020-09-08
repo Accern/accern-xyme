444e1db7c2af353c32f91d1e63563212084e85ab
Yue(Joy) Shangguan <35748429+ys3244@users.noreply.github.com>
2020-09-08 16:23:57 -0400

    comma added

--- accern_xyme/accern_xyme.py
+++ accern_xyme/accern_xyme.py
@@ -782,7 +782,7 @@ class PipelineHandle:
         pipe_ids = self.get_id()
         pipe_obj = pipe_timing.get(pipe_ids, {
             "pipe_total": 0.0,
-            "node": node_timing_sorted
+            "node": node_timing_sorted,
         })
         pipe_obj["pipe_total"] += float(pipe_sums)
         pipe_timing[pipe_ids] = pipe_obj
