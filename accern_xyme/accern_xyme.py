f756a10094dd7d5cd300cbe46aefd01377291f23
Yuxiang Zhang <rico.zhang9318@gmail.com>
2020-09-09 18:49:21 -0400

    post

--- accern_xyme/accern_xyme.py
+++ accern_xyme/accern_xyme.py
@@ -857,11 +857,11 @@ class PipelineHandle:
 
     def get_dynamic_status(
             self,
-            data_ids: List[ComputationHandle]) -> Dict[
+            data_ids: List['ComputationHandle']) -> Dict[
                 'ComputationHandle', QueueStatus]:
         data_ids_str = ",".join([data_id.get_id() for data_id in data_ids])
         res = cast(DynamicStatusResponse, self._client._request_json(
-            METHOD_GET, "/dynamic_status", {
+            METHOD_POST, "/dynamic_status", {
                 "data_ids": data_ids_str,
                 "pipeline": self._pipe_id,
             }, capture_err=True))
