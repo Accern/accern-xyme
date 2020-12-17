7074228668f15fbf05f9581fa0e26845ff75f194
sean <seandbergen@gmail.com>
2020-12-17 09:32:12 -0500

    wrote functions to set name, company, state, high_priority, and queue_mng fields for a PipeLineHandler

--- accern_xyme/accern_xyme.py
+++ accern_xyme/accern_xyme.py
@@ -1375,6 +1375,31 @@ class PipelineHandle:
         pipe_def[attr] = value
         self = self._client.set_pipeline(self.get_id(), pipe_def)
 
+    def set_name(
+            self,
+            value: str) -> None:
+        self.set_attr("name", value)
+
+    def set_company(
+            self,
+            value: str) -> None:
+        self.set_attr("company", value)
+
+    def set_state(
+            self,
+            value: str) -> None:
+        self.set_attr("state", value)
+
+    def set_high_priority(
+            self,
+            value: bool) -> None:
+        self.set_attr("high_priority", value)
+
+    def set_queue_mng(
+            self,
+            value: str) -> None:
+        self.set_attr("queue_mng", value)
+
     def get_visible_blobs(self) -> List[str]:
         return [
             res[0]
