1f6c58c7433ee906b5deffbaad6bdb8e8975944c
Juliana Barbosa <julesbarbosa1@gmail.com>
2021-03-24 10:12:22 -0400

    moving version

--- accern_xyme/__init__.py
+++ accern_xyme/__init__.py
@@ -1,4 +1,5 @@
 # pylint: disable=unused-import
+__version__ = '0.2.3'
 import accern_xyme.v2
 import accern_xyme.v3
 import accern_xyme.accern_xyme
@@ -6,6 +7,5 @@ import accern_xyme.types
 import accern_xyme.util
 
 from accern_xyme.accern_xyme import *
-from accern_xyme.accern_xyme import __version__
 from accern_xyme.types import *
 from accern_xyme.util import *
