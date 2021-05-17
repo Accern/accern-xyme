73a58ccf502b1805f3fd66d73264762c05472e94
Yuxiang Zhang <rico.zhang9318@gmail.com>
2021-05-17 18:46:40 -0400

    lint

--- packages/typescript/index.ts
+++ packages/typescript/index.ts
@@ -2758,13 +2758,13 @@ export class BlobHandle {
         );
     }
 
-    public async convertModel(reload=true): Promise<ModelReleaseResponse> {
+    public async convertModel(reload = true): Promise<ModelReleaseResponse> {
         return await this.client.requestJSON<ModelReleaseResponse>({
             method: METHOD_POST,
             path: '/convert_model',
             args: {
                 blob: this.getURI(),
-                reload
+                reload,
             },
         });
     }
