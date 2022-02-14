6186a193cd47d66babb968a9d6e8dbb8efcc6393
TheAnkurGoswami <kinglusifer1401@gmail.com>
2022-02-14 12:21:26 +0530

    lints

--- packages/python/accern_xyme/types.py
+++ packages/python/accern_xyme/types.py
@@ -23,6 +23,8 @@ UserDagDef = TypedDict('UserDagDef', {
     "uri_prefix": URIPrefix,
     "state_uri": str,
 }, total=False)
+
+
 class DagDef(UserDagDef):
     company: str
     default_input_key: Optional[str]
@@ -33,6 +35,8 @@ class DagDef(UserDagDef):
     queue_mng: Optional[str]
     uri_prefix: URIPrefix
     state_uri: str
+
+
 NodeDef = TypedDict('NodeDef', {
     "blobs": Dict[str, str],
     "id": str,
